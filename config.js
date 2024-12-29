a1a37361bb78900870567f8150ca2a33bf822de2
DasunMax970 <dasunmax08@gmail.com>
2024-12-29 08:22:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? 'SEON-MD=aA9yjAZY#FI3q-MR2vS1k5ApHlWSf7M6Qhov_tMRM8xuIQ5I2VsI' : process.env.SESSION_ID,
-GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'uvindugit': process.env.GITHUB_USERNAME,
+GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? '': process.env.GITHUB_USERNAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'Put your github auth token': process.env.GITHUB_AUTH_TOKEN
 };
